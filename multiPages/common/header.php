ba514b3483b1a112c95d2596c2bf039eadef3a7d
Sujan Ch <104439396+Sujan299@users.noreply.github.com>
2024-06-02 00:14:23 +0545

    Add files via upload

---
+++ multiPages/common/header.php
@@ -0,0 +1,15 @@
+<div className='firstNav'>
+        <div>Logo</div>
+        <div>Welcome to my site ! </div>
+    </div>
+<header>
+        <nav>
+            <ul>
+                <li><a href="./home.php">Home</a></li>
+                <li><a href="./about.php">About Us</a></li>
+                <li><a href="./contact.php">Contact us </a></li>
+                <li><a href="./login.php">Login</a></li>
+                <li><a href="./signup.php">signup</a></li>
+            </ul>
+</nav>
+</header>
